e031455467ac7a7ab6a45077ab1217d4d48d48ec
surya pratap <thakursuryapratap9@gmail.com>
2023-07-27 13:41:39 +0530

    final

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# food-comparison-app
+Create A food Comparison app.  Where you take two food items and trace the similarity and difference in their few traits like, texture, odor, flavour, ingredients, cooking methods involved.
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [food1, setFood1] = useState('');
+  const [food2, setFood2] = useState('');
+  const [comparisonResult, setComparisonResult] = useState(null);
+
+  const handleComparison = () => {
+    // Perform comparison logic here (e.g., API calls, data processing)
+    // For simplicity, let's assume we have a pre-defined comparisonResult object
+
+    const mockComparisonResult = {
+      texture: 'Both are soft and chewy',
+      odor: 'Food 1 has a stronger aroma',
+      flavour: 'Food 2 is spicier',
+      ingredients: 'Food 1 contains nuts, while Food 2 does not',
+      cookingMethods: 'Both are fried',
+    };
+
+    setComparisonResult(mockComparisonResult);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Food Comparison App</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Food 1"
+          value={food1}
+          onChange={(e) => setFood1(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter Food 2"
+          value={food2}
+          onChange={(e) => setFood2(e.target.value)}
+        />
+        <button onClick={handleComparison}>Compare</button>
+      </div>
+      {comparisonResult && (
+        <div>
+          <h2>Comparison Result</h2>
+          <p>Texture: {comparisonResult.texture}</p>
+          <p>Odor: {comparisonResult.odor}</p>
+          <p>Flavour: {comparisonResult.flavour}</p>
+          <p>Ingredients: {comparisonResult.ingredients}</p>
+          <p>Cooking Methods: {comparisonResult.cookingMethods}</p>
+        </div>
+      )}
     </div>
   );
 }
